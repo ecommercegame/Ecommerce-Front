d34fe8f1b18279c34431c8d8f450c7dfb1b4a80e
keelcoutinho <kelvia_cou@hotmail.com>
2022-06-07 20:01:03 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
+import { DestaqueComponent } from './components/destaque/destaque.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    CarrosselComponent,
+    DestaqueComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
