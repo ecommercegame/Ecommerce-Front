7b28d08c44fe9fdbfeec63a9f452f21d1483bfe5
Jefferson da Silva <je_jss@hotmail.com>
2022-07-01 09:04:11 -0300

    update routes to backend

--- src/app/service/produtos.service.ts
+++ src/app/service/produtos.service.ts
@@ -25,16 +25,11 @@ export class ProdutosService {
   }
 
   getAllProdutos():Observable<Produtos[]>{
-<<<<<<< HEAD
     return this.http.get<Produtos[]>(`${this.produtosURL}`)
-=======
-    return this.http.get<Produtos[]>('http://eccomerce-pi.herokuapp.com/produtos')
->>>>>>> 3fb33057fc119cff8f36d7d4800f9f61a49ccd91
 
   }
 
   getProdutoById(id: number): Observable<Produtos> {
-<<<<<<< HEAD
     return this.http.get<Produtos>(`${this.produtosURL}/${id}`)
   }
 
@@ -44,42 +39,22 @@ export class ProdutosService {
 
   getProdutosByCategoria(categorias: string): Observable<Produtos[]>{
     return this.http.get<Produtos[]>(`${this.produtosURL}/categorias/${categorias}`)
-=======
-    return this.http.get<Produtos>(`http://eccomerce-pi.herokuapp.com/produtos/${id}`)
-  }
 
-  getProdutosByNome(nome: string): Observable<Produtos[]>{
-    return this.http.get<Produtos[]>(`http://eccomerce-pi.herokuapp.com/produtos/nome/${nome}`)
-  }
-
-  getProdutosByCategoria(categorias: string): Observable<Produtos[]>{
-    return this.http.get<Produtos[]>(`http://eccomerce-pi.herokuapp.com/produtos/categorias/${categorias}`)
->>>>>>> 3fb33057fc119cff8f36d7d4800f9f61a49ccd91
   }
 
 
   postProdutos(produtos:Produtos):Observable<Produtos>{
-<<<<<<< HEAD
     return this.http.post<Produtos>(`${this.produtosURL}/cadastrar`, produtos, this.token)
   }
 
   putProdutos(produtos: Produtos):Observable<Produtos>{
     return this.http.put<Produtos>(`${this.produtosURL}/atualizar`, produtos, this.token)
-=======
-    return this.http.post<Produtos>('http://eccomerce-pi.herokuapp.com/produtos/cadastrar', produtos, this.token)
-  }
 
-  putProdutos(produtos: Produtos):Observable<Produtos>{
-    return this.http.put<Produtos>('http://eccomerce-pi.herokuapp.com/produtos/atualizar', produtos, this.token)
->>>>>>> 3fb33057fc119cff8f36d7d4800f9f61a49ccd91
   }
 
 
   deletarProduto(id: number): Observable<Produtos>{
-<<<<<<< HEAD
     return this.http.delete<Produtos>(`${this.produtosURL}/${id}`, this.token)
-=======
-    return this.http.delete<Produtos>(`http://eccomerce-pi.herokuapp.com/produtos/${id}`, this.token)
->>>>>>> 3fb33057fc119cff8f36d7d4800f9f61a49ccd91
+
   }
 }
